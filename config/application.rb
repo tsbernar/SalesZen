475d82903e5e7fc13c9b3e996c1bab90519ffa55
Trevor Bernard <trevor.s.bernard@gmail.com>
2016-03-04 22:04:28 -0800

    config for bundle using correct environment

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Require the gems listed in Gemfile for the appropriate environment
+Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module SalesZen
   class Application < Rails::Application
