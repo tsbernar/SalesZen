8b3580af69efa3181feee9f28593719b9c8cef7e
Trevor Bernard <trevor.s.bernard@gmail.com>
2016-03-08 21:56:14 -0800

    added controller for users, and view for user account page, redirect to user account page after updating account settings in registration edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
+  get 'users/:id' => 'users#show', as: 'user'
+
   resources :contacts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305091845) do
+ActiveRecord::Schema.define(version: 20160309050801) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160305091845) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
